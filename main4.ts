1c189f5205584c012f3bd2eede4a26e15b421a30
blkcor <1029129867@qq.com>
2023-02-01 16:18:03 +0800

    chore:modify file name

--- main4.ts
+++ main4.ts
@@ -1,54 +1,47 @@
-import { Build } from './main3';
+//uppercase a letter in union type
+type UpperCaseA<Item extends string> = Item extends 'a' ? Uppercase<Item> : Item
+type union1 = 'a' | 'b' | 'c'
+type result1 = UpperCaseA<union1>
 
-//Add
-type Add<Num1 extends number, Num2 extends number> = [...Build<Num1, 1>, ...Build<Num2, 2>]['length']
-type result1 = Add<1, 2>
+//Camelcase Union
+type CalmelcaseUnion<Item extends string> = Item extends `${infer left}_${infer right}${infer rest}` ?
+  `${left}${Uppercase<right>}${rest}` : never
 
-//Sub
-type Subtract<Num1 extends number, Num2 extends number> = Build<Num1> extends [...Build<Num2>, ...infer Rest] ? Rest['length'] : never
-type result2 = Subtract<32, 23>
+type union2 = 'aa_aa' | 'cc_cc' | 'bb_bb'
+type result2 = CalmelcaseUnion<union2>
 
-//Mutiple
-type Multiple<Num1 extends number, Num2 extends number, ResultArr extends unknown[] = []> =
-  Num2 extends 0 ? ResultArr['length'] :
-  Multiple<Num1, Subtract<Num2, 1>, [...Build<Num1>, ...ResultArr]>
-type result3 = Multiple<2, 9>
+// IsUnion
+// A extends A是为了触发分布式条件判断
+// [B] extends [A]是为了不触发分布式条件判断
+//分布式条件判断是联合类型独有的特性
+//若传入 'a'& 'b' 永远会返回false
+type IsUnion<A, B = A> = A extends A ?
+  [B] extends [A] ?
+  false : true : never
+type union3 = 'a' | 'b'
+type result3 = IsUnion<union3>
 
-//devide
-type Divide<Num1 extends number, Num2 extends number, ResultArr extends unknown[] = []> =
-  Num1 extends 0 ? ResultArr['length'] :
-  Divide<Subtract<Num1, Num2>, Num2, [unknown, ...ResultArr]>
-type result4 = Divide<50, 10>
+//array to union
+type union4 = ['aaa', 'bbb'][number]
 
-//StrLen
-type StrLen<Str extends string, CountArr extends unknown[] = []> = Str extends `${string}${infer Rest}` ? StrLen<Rest, [unknown, ...CountArr]> : CountArr['length']
-type result5 = StrLen<'Do U Love Me?'>
+//于是可以这样实现BEM   block_element--modifier
+type BEMResult<
+  block extends string,
+  element extends string[],
+  modifier extends string[]
+> = `${block}_${element[number]}--${modifier[number]}`
+type result4 = BEMResult<"blkcor", ["name", "age"], ["chy", "love"]>
 
-//GreaterThan
-// type GreaterThan<Num1 extends number, Num2 extends number, CountArr extends unknown[] = []> =
-//   Num1 extends Num2 ? false :
-//   CountArr['length'] extends Num2 ? true :
-//   CountArr['length'] extends Num1 ? false :
-//   GreaterThan<Num1, Num2, [unknown, ...CountArr]>
-type GreaterThan<Num1 extends number, Num2 extends number> =
-  Build<Num1> extends [...Build<Num2>, ...infer Rest] ?
-  Rest['length'] extends 0 ?
-  false
-  : true
-  : false
-type result6 = GreaterThan<1, 2>
-
+//AllCombinations example:传入 'A' | 'B' 的时候，能够返回所有的组合： 'A' | 'B' | 'BA' | 'AB'。
+//first we accomplish Combination for two letters
+type Combination<A extends string, B extends string> = | A | B | `${A}${B}` | `${B}${A}`
+//then we accomplish AllCombination besed on the Combination
+type AllCombination<A extends string, B extends string = A> = A extends A ?
+  Combination<A, AllCombination<Exclude<B, A>>> : never
+type result5 = AllCombination<'a' | 'v' | 's'>
 
 
-//Fibonacci
-type FibonacciLoop<
-  PrevArr extends unknown[],
-  CurrentArr extends unknown[],
-  IndexArr extends unknown[],
-  Num extends number
-> = Num extends IndexArr['length'] ? CurrentArr['length'] :
-  FibonacciLoop<CurrentArr, [...PrevArr, ...CurrentArr], [...IndexArr, unknown], Num>
-type Fibonacci<Num extends number> = FibonacciLoop<[1], [], [], Num>
 
-type result7 = Fibonacci<9>
+
+
 
--- main5.ts
+++ main5.ts
@@ -1,47 +1,112 @@
-//uppercase a letter in union type
-type UpperCaseA<Item extends string> = Item extends 'a' ? Uppercase<Item> : Item
-type union1 = 'a' | 'b' | 'c'
-type result1 = UpperCaseA<union1>
+//isAny 
+type isAny<T> = 'blkcor' extends ('chy' & T) ? true : false
+type r1 = isAny<'dd'>
 
-//Camelcase Union
-type CalmelcaseUnion<Item extends string> = Item extends `${infer left}_${infer right}${infer rest}` ?
-  `${left}${Uppercase<right>}${rest}` : never
+//more robust isEqual
+type isEqual2<A, B> = (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? true : false
+type r2 = isEqual2<1, any>
 
-type union2 = 'aa_aa' | 'cc_cc' | 'bb_bb'
-type result2 = CalmelcaseUnion<union2>
+//isUnion 
+type isUnion<A, B = A> = A extends A ? [B] extends [A] ? false : true : never
+type r3 = isUnion<'a' | 'b'>
 
-// IsUnion
-// A extends A是为了触发分布式条件判断
-// [B] extends [A]是为了不触发分布式条件判断
-//分布式条件判断是联合类型独有的特性
-//若传入 'a'& 'b' 永远会返回false
-type IsUnion<A, B = A> = A extends A ?
-  [B] extends [A] ?
-  false : true : never
-type union3 = 'a' | 'b'
-type result3 = IsUnion<union3>
+//IsNever
+//never比较特殊 如果条件类型参数左边为never 那么直接会返回never
+//因此我们需要特殊处理一下
+type isNever<T> = [T] extends [never] ? true : false
+type r4 = isNever<'a'>
 
-//array to union
-type union4 = ['aaa', 'bbb'][number]
+//除此以外，any 在条件类型中也比较特殊，如果类型参数为 any，会直接返回 trueType 和 falseType 的合并 (联合类型)
+type TestAny<T> = T extends number ? 1 : 2
+type r5 = TestAny<any>
 
-//于是可以这样实现BEM   block_element--modifier
-type BEMResult<
-  block extends string,
-  element extends string[],
-  modifier extends string[]
-> = `${block}_${element[number]}--${modifier[number]}`
-type result4 = BEMResult<"blkcor", ["name", "age"], ["chy", "love"]>
+//IsTuple
+type NotEqual<A, B> =
+  (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2)
+  ? false : true;
+type IsTuple<T> = T extends [...params: infer Elems] ? NotEqual<Elems['length'], number> : false
+type r6 = IsTuple<number[]>
 
-//AllCombinations example:传入 'A' | 'B' 的时候，能够返回所有的组合： 'A' | 'B' | 'BA' | 'AB'。
-//first we accomplish Combination for two letters
-type Combination<A extends string, B extends string> = | A | B | `${A}${B}` | `${B}${A}`
-//then we accomplish AllCombination besed on the Combination
-type AllCombination<A extends string, B extends string = A> = A extends A ?
-  Combination<A, AllCombination<Exclude<B, A>>> : never
-type result5 = AllCombination<'a' | 'v' | 's'>
+//UnionToInterSection
+//在 TypeScript 中有函数参数是有逆变的性质的，也就是如果参数可能是多个类型，参数类型会变成它们的交叉类型。
+type UnionToInterSection<U> = (U extends U ? (x: U) => unknown : never) extends (x: infer R) => unknown ? R : never
+type r7 = UnionToInterSection<{ a: 1 } | { b: 2 }>
+
+//GetOptional
+type GetOptional<Obj extends Record<string, any>> = {
+  [key in keyof Obj as {} extends Pick<Obj, key> ? key : never]: Obj[key]
+}
+type r8 = GetOptional<{
+  name: 'blkcor',
+  age?: 12
+}>
+
+//GetRequired
+type GetRequired<Obj extends Record<string, any>> = {
+  [key in keyof Obj as {} extends Pick<Obj, key> ? never : key]: Obj[key]
+}
+type r9 = GetRequired<
+  {
+    name: 'blkcor',
+    age?: 12
+  }
+>
+
+//RemoveIndexSignature
+//什么是索引签名?
+/**
+ * type Dong = {
+ * [key: string]: any;
+ *  sleep(): void;
+    }
+ * 这里的[key:string]:any就是索引签名  表示可以添加任意 键为string 值为any类型的索引
+ */
+type RemoveIndexSignature<Obj extends Record<string, any>> = {
+  [key in keyof Obj as key extends `${infer Str}` ? Str : never]: Obj[key]
+}
+type r10 = RemoveIndexSignature<
+  {
+    [key: string]: any,
+    a: () => any,
+    b(): void
+  }
+>
+
+//ClassPublicProps  
+//keyof进行遍历的时候只能拿到public的属性
+class Person {
+  public name: string
+  private age: number
+  protected hobbies: string[]
+
+  constructor() {
+    this.name = 'dong';
+    this.age = 20;
+    this.hobbies = ['sleep', 'eat'];
+  }
+}
+
+type ClassPublicProps<Obj extends Record<string, any>> = {
+  [key in keyof Obj]: Obj[key]
+}
+type r11 = ClassPublicProps<Person>
 
 
+//as const
+//ts 推导出来的类型默认并不是字面量类型
+const obj = {
+  a: 1,
+  b: 2
+}
+type t = typeof obj
 
+//那么我们如何推导出字面量类型呢？ ==> 使用 as const
+const obj2 = {
+  a: 1,
+  b: 2
+} as const
+type t2 = typeof obj2
+//但是加上 as const 之后推导出来的类型是带有 readonly 修饰的，所以再通过模式匹配提取类型的时候也要加上 readonly 的修饰才行。
 
 
 
